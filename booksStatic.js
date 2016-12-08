9e5e440d095750103322bbfa8ce1264b0341c8df
Ben Logan <benlogan1981@gmail.com>
2016-12-08 09:49:49 +0000

    featured books cache

--- booksStatic.js
+++ booksStatic.js
@@ -1,48 +1,55 @@
 var books = require('./books.js');
 
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache({ checkperiod: 7200 }); // this can be a long life cache, it never changes! 2hrs
+
 exports.featuredBooks = function (bookCategory, response) {
     console.log('About to execute featured book lookup!');
 
-    var parsedJSON;
-    switch (bookCategory) {
-        case "programming":
-            parsedJSON = require('./data/programming.json');
-            break;
-        case "management":
-            parsedJSON = require('./data/management.json');
-            break;
-        case "classics":
-            parsedJSON = require('./data/classics.json');
-            break;
-        case "searched":
-            parsedJSON = require('./data/searched.json');
-            break;
-        case "satire":
-            parsedJSON = require('./data/satire.json');
-            break;
-    }
-
-    if(parsedJSON) {
-        //console.log(parsedJSON);
-
-        var asinCommaList = '';
-        
-        for(var bookIndex in parsedJSON.bookList) {
-            asinCommaList += parsedJSON.bookList[bookIndex].asin + ',';
-        }
-        asinCommaList = asinCommaList.substring(0, asinCommaList.length - 1);
-
-        var booksData = [];
-
-        books.amazonBookLookupOnly(asinCommaList, function(result) { 
-            for(var i = 0; i < result.length; i++) {
-                var book = { "title":result[i].book.title, "asin":result[i].book.asin,  "url":result[i].book.url, "author":result[i].book.author, "isbn":result[i].book.isbn};
-                booksData.push(book);
-                
-            }
-            response.end(JSON.stringify(booksData));
-        });
+    var cacheValue = myCache.get("featuredBooks_" + bookCategory);
+    if(cacheValue != undefined) {
+      response.end(JSON.stringify(cacheValue));
     } else {
-        response.end();
+      var parsedJSON;
+      switch (bookCategory) {
+          case "programming":
+              parsedJSON = require('./data/programming.json');
+              break;
+          case "management":
+              parsedJSON = require('./data/management.json');
+              break;
+          case "classics":
+              parsedJSON = require('./data/classics.json');
+              break;
+          case "searched":
+              parsedJSON = require('./data/searched.json');
+              break;
+          case "satire":
+              parsedJSON = require('./data/satire.json');
+              break;
+      }
+
+      if(parsedJSON) {
+          var asinCommaList = '';
+
+          for(var bookIndex in parsedJSON.bookList) {
+              asinCommaList += parsedJSON.bookList[bookIndex].asin + ',';
+          }
+          asinCommaList = asinCommaList.substring(0, asinCommaList.length - 1);
+
+          var booksData = [];
+
+          books.amazonBookLookupOnly(asinCommaList, function(result) {
+              for(var i = 0; i < result.length; i++) {
+                  var book = { "title":result[i].book.title, "asin":result[i].book.asin,  "url":result[i].book.url, "author":result[i].book.author, "isbn":result[i].book.isbn};
+                  booksData.push(book);
+
+              }
+              myCache.set("featuredBooks_" + bookCategory, booksData);
+              response.end(JSON.stringify(booksData));
+          });
+      } else {
+          response.end();
+      }
     }
 }
